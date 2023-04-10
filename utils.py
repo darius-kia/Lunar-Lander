cf524a8c367ac436bcb761ac2daf6f05a4ad7478
Darius <dariuskianersi@gmail.com>
2023-04-10 16:54:25 -0400

    Upload utils.py

---
+++ utils.py
@@ -0,0 +1,154 @@
+
+import base64
+import random
+from itertools import zip_longest
+
+import imageio
+import IPython
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from statsmodels.iolib.table import SimpleTable
+
+SEED = 0  # Seed for the pseudo-random number generator.
+MINIBATCH_SIZE = 64  # Mini-batch size.
+TAU = 1e-3  # Soft update parameter.
+E_DECAY = 0.995  # ε-decay rate for the ε-greedy policy.
+E_MIN = 0.01  # Minimum ε value for the ε-greedy policy.
+
+
+random.seed(SEED)
+
+
+def get_experiences(memory_buffer):
+    experiences = random.sample(memory_buffer, k=MINIBATCH_SIZE)
+    states = tf.convert_to_tensor(
+        np.array([e.state for e in experiences if e is not None]), dtype=tf.float32
+    )
+    actions = tf.convert_to_tensor(
+        np.array([e.action for e in experiences if e is not None]), dtype=tf.float32
+    )
+    rewards = tf.convert_to_tensor(
+        np.array([e.reward for e in experiences if e is not None]), dtype=tf.float32
+    )
+    next_states = tf.convert_to_tensor(
+        np.array([e.next_state for e in experiences if e is not None]), dtype=tf.float32
+    )
+    done_vals = tf.convert_to_tensor(
+        np.array([e.done for e in experiences if e is not None]).astype(np.uint8),
+        dtype=tf.float32,
+    )
+    return (states, actions, rewards, next_states, done_vals)
+
+
+def check_update_conditions(t, num_steps_upd, memory_buffer):
+
+    if (t + 1) % num_steps_upd == 0 and len(memory_buffer) > MINIBATCH_SIZE:
+        return True
+    else:
+        return False
+
+
+def get_new_eps(epsilon):
+    return max(E_MIN, E_DECAY * epsilon)
+
+
+def get_action(q_values, epsilon=0.0):
+   if random.random() > epsilon:
+        return np.argmax(q_values.numpy()[0])
+    else:
+        return random.choice(np.arange(4))
+
+
+def update_target_network(q_network, target_q_network):
+    for target_weights, q_net_weights in zip(
+        target_q_network.weights, q_network.weights
+    ):
+        target_weights.assign(TAU * q_net_weights + (1.0 - TAU) * target_weights)
+
+
+def plot_history(point_history, **kwargs):
+    lower_limit = 0
+    upper_limit = len(point_history)
+
+    window_size = (upper_limit * 10) // 100
+
+    plot_rolling_mean_only = False
+    plot_data_only = False
+
+    if kwargs:
+        if "window_size" in kwargs:
+            window_size = kwargs["window_size"]
+
+        if "lower_limit" in kwargs:
+            lower_limit = kwargs["lower_limit"]
+
+        if "upper_limit" in kwargs:
+            upper_limit = kwargs["upper_limit"]
+
+        if "plot_rolling_mean_only" in kwargs:
+            plot_rolling_mean_only = kwargs["plot_rolling_mean_only"]
+
+        if "plot_data_only" in kwargs:
+            plot_data_only = kwargs["plot_data_only"]
+
+    points = point_history[lower_limit:upper_limit]
+
+    # Generate x-axis for plotting.
+    episode_num = [x for x in range(lower_limit, upper_limit)]
+
+    # Use Pandas to calculate the rolling mean (moving average).
+    rolling_mean = pd.DataFrame(points).rolling(window_size).mean()
+
+    plt.figure(figsize=(10, 7), facecolor="white")
+
+    if plot_data_only:
+        plt.plot(episode_num, points, linewidth=1, color="cyan")
+    elif plot_rolling_mean_only:
+        plt.plot(episode_num, rolling_mean, linewidth=2, color="magenta")
+    else:
+        plt.plot(episode_num, points, linewidth=1, color="cyan")
+        plt.plot(episode_num, rolling_mean, linewidth=2, color="magenta")
+
+    text_color = "black"
+
+    ax = plt.gca()
+    ax.set_facecolor("black")
+    plt.grid()
+    plt.xlabel("Episode", color=text_color, fontsize=30)
+    plt.ylabel("Total Points", color=text_color, fontsize=30)
+    yNumFmt = mticker.StrMethodFormatter("{x:,}")
+    ax.yaxis.set_major_formatter(yNumFmt)
+    ax.tick_params(axis="x", colors=text_color)
+    ax.tick_params(axis="y", colors=text_color)
+    plt.show()
+
+
+def display_table(initial_state, action, next_state, reward, done):
+    action_labels = [
+        "Do nothing",
+        "Fire right engine",
+        "Fire main engine",
+        "Fire left engine",
+    ]
+
+    # Do not use column headers.
+    column_headers = None
+
+    # Display all floating point numbers rounded to 3 decimal places.
+    with np.printoptions(formatter={"float": "{:.3f}".format}):
+        table_info = [
+            ("Initial State:", [f"{initial_state}"]),
+            ("Action:", [f"{action_labels[action]}"]),
+            ("Next State:", [f"{next_state}"]),
+            ("Reward Received:", [f"{reward:.3f}"]),
+            ("Episode Terminated:", [f"{done}"]),
+        ]
+
+    # Generate table.
+    row_labels, data = zip_longest(*table_info)
+    table = SimpleTable(data, column_headers, row_labels)
+
+    return table
